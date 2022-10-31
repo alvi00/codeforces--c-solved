bb2ee19100c5d1bb175658205c07c00b65c0fd05
Ahmad Fahmid <112432485+alvi00@users.noreply.github.com>
2022-10-31 13:18:54 +0600

    Update watermelon.cpp

--- watermelon.cpp
+++ watermelon.cpp
@@ -1,30 +1,17 @@
+//submitted in codeforces and been accepted
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-int a;
- cout<<"Enter the weight"<<endl;
+ int a;
  cin>>a;
- if(a>=0 && a<=100)
+ if(a%2==0 && a>2)
     {
-        if(a==2)
-        {
-            cout<<"No"<<endl;
-        }
-        else if(a%2==0)
-        {
             cout<<"YES"<<endl;
-        }
-        else
-        {
-            cout<<"NO"<<endl;
-        }
-
     }
     else
     {
-        cout<<"Enter number between o and 100"<<endl;
+        cout<<"NO"<<endl;
     }
-
+return 0;
 }
-
