8dafd02c73de2fe7ba796b92b23492f121e96508
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2022-12-19 02:06:59 +0600

    Add files via upload

---
+++ 1772A+b.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve();
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--)
+   {
+      solve();
+   }
+   return 0;
+}
+void solve()
+{
+    int a,b;
+    cin>>a>>b;
+    int c=a+b;
+    cout<<c<<endl;
+}
