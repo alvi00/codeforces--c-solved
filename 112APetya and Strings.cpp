b52ac8ecff565e8371c62ace5c2ab054317e1314
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2023-01-02 23:11:43 +0600

    Add files via upload

---
+++ 112APetya and Strings.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char arr[100];
+    char arr1[100];
+    gets(arr);
+    cout<<"\n";
+    gets(arr1);
+    int a=strcmpi(arr,arr1);
+    cout<<a;
+    return 0;
+
+}
