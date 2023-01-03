83f73555be6a0d7473cf492c8ee22a9cefe804cb
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2023-01-03 14:17:13 +0600

    Add files via upload

---
+++ 281AWord Capitalization.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    if(s[0]>='a' && s[0]<='z')
+    {
+        s[0]=s[0]-32;
+        cout<<s;
+    }
+    else
+    {
+        cout<<s;
+    }
+
+  return 0;
+}
+
