5d66c431fe8604eb719a8826c66c2241589c186c
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2023-01-05 13:35:34 +0600

    Add files via upload

---
+++ 200BDrinks.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+     int n;
+     float sum=0.00;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+        for(int i=0;i<n;i++)
+        {
+            sum=sum+arr[i];
+        }
+
+cout<<sum/n;
+
+    return 0;
+} 
