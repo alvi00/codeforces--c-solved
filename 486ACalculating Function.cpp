a03eb4d97c0a8edbad87d66cb3659186f5bdb325
Ahmad Fahmid <ahmadfahmid59@gmail.com>
2023-01-05 13:01:18 +0600

    Add files via upload

---
+++ 486ACalculating Function.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int  n;
+    cin>>n;
+    long long int a;
+        if(n%2==0)
+        {
+            cout<<n/2;
+        }
+        else
+        {
+            a=-(n/2+1);
+            cout<<a;
+                 }
+    
+
+
+} 
